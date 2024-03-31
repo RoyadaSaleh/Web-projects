54915f80901cdafe34cb6728a7a6fae6823aab7c
RoyadaSaleh <royada.saleh@gmail.com>
2024-03-31 14:51:14 -0400

    Initial commit

---
+++ social-/src/components/Form.jsx
@@ -0,0 +1,18 @@
+function Form(){
+    return(
+        <form>
+            <label htmlFor="linkName">Link Name:</label>
+            <br/>
+            <input type="text" id="linkName" name="linkName" style={{width:"100%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}} value={name} onChange={(newVal)=>setName(newVal.target.value)}/>
+            <br />
+            <br />
+            <label htmlFor="URL">Link URL:</label>
+            <input type="text" id="linkURL" name="linkURL" value={URL} onChange={(newVal)=>setURL(newVal.target.value)} style={{width:"100%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+            <br/>
+            <br />
+            <input type="submit" value="Submit" onClick={(value)=>submitForm(value, props.handleSubmit)}  style={{width:"7rem", height:"2rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}></input>
+        </form>
+    ) 
+
+}
+export default Form
---
+++ social-/src/components/Table.jsx
@@ -0,0 +1,11 @@
+function Table(){
+    return (
+        <Table>
+            <tr>
+                <th>Link name</th>
+                <th>Link URL</th>
+            </tr>
+        </Table>
+    )
+}
+export default Table
