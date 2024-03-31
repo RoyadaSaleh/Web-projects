f1ea019efe17cfd200fa56d5d5b23ac8c36364db
RoyadaSaleh <royada.saleh@gmail.com>
2024-03-31 15:29:31 -0400

    few edits

--- social-/src/components/Form.jsx
+++ social-/src/components/Form.jsx
@@ -1,16 +1,14 @@
 function Form(){
     return(
         <form>
-            <label htmlFor="linkName">Link Name:</label>
-            <br/>
-            <input type="text" id="linkName" name="linkName" style={{width:"100%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}} value={name} onChange={(newVal)=>setName(newVal.target.value)}/>
+            <label for="linkName">Link Name:</label>
+            <input type="text" name="linkName"/>
             <br />
-            <br />
-            <label htmlFor="URL">Link URL:</label>
-            <input type="text" id="linkURL" name="linkURL" value={URL} onChange={(newVal)=>setURL(newVal.target.value)} style={{width:"100%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+            <label for="URL">Link URL:</label>
+            <input type="text" name="linkName"/>
             <br/>
             <br />
-            <input type="submit" value="Submit" onClick={(value)=>submitForm(value, props.handleSubmit)}  style={{width:"7rem", height:"2rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}></input>
+            <input type="submit" />
         </form>
     )  
 
