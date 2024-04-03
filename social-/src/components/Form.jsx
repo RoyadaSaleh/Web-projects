74b93ebf30438262196b6e72b5700819907b48c0
RoyadaSaleh <royada.saleh@gmail.com>
2024-04-03 17:16:01 -0400

    created new array

--- social-/src/components/Form.jsx
+++ social-/src/components/Form.jsx
@@ -1,13 +1,29 @@
+import {useState} from 'react'
+
 function Form(){
+    const [postTitle, setPostTitle] = useState("")
+
+    const handleChange = (event) => {
+        setPostTitle(event.target.value)
+
+    }
+    
     return(
-        <form>
+        <form onSubmit={(event)=>{
+            event.preventDefault() 
+
+            if (postTitle !== "") {
+                
+            }else {
+                alert("Posts need a comment")
+            }
+
+        }}>
             <label for="linkName">Link Name:</label>
             <input type="text" name="linkName"/>
             <br />
             <label for="URL">Link URL:</label>
-            <input type="text" name="linkName"/>
-            <br/>
-            <br />
+            <input type="text" name="linkName" onChange={handleChange} value={postTitle}/>
             <input type="submit" />
         </form>
     )  
